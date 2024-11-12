59739b0fe09a229df04b5830ea873926e9e2821c
egrundberg-27 <elgr7725@colorado.edu>
2024-11-12 01:53:30 -0700

    Add files via upload
    Low key dont know how much is correct, graph seems accurate for depth data? But my eigenvalue stuff seems way off and I couldn't figure out how to get it into a 1 x 1 (might've been overthinking it). Also apologies if my variable naming is shit. I fixed a lot of it and I was tired.

---
+++ Project2Part2_2_2360.m
@@ -0,0 +1,60 @@
+clear;
+clc;
+close all;
+
+A = readmatrix('mariana_depth.csv');
+%dataLat = readmatrix('mariana_latitude.csv');
+%dataLong = readmatrix('mariana_longitude.csv');
+
+
+%% Part 1
+tic
+%Transpose A
+A_T = A';
+%Create Unit vector
+n = 1;
+x = randn(n,1);
+x = x / norm(x);
+
+%Iterate until the unit vector for n+1 minus the previous unit vector
+%doesn't change
+for i = 1:10
+unit_x = A_T*A*x;
+unit_x = unit_x / (norm(unit_x));
+
+if  norm(unit_x - x) <= 0
+break;
+end
+
+x = unit_x;
+
+end
+%Show final eigenvalue and eigenvector
+eigen_final = x;
+k = eig(A'*A);
+x_n_vector = sqrt(sum(x^2));
+N = length(x);
+hold on;
+plot(1:N, x_n_vector, 'k-');
+toc
+%% Part 2
+
+v_50 = zeros(50,1);
+
+%Initialize a unit vector
+n = 1;
+u = randn(n,1);
+u = u / norm(u);
+% 
+for i = 1:50
+u_new_asterisk = A'*A*u;
+u_new = u_new_asterisk - sum((u_new_asterisk'*v_50(i)*v_50(i)));
+u_new = u_new / norm(u_new);
+if norm(u_new - u) < 0.002
+break;
+end
+
+u = u_new;
+end
+
+v_i = u_new;
